8bd464892d008005fbc66a5d7bd1954cffa6d9a5
Jim Borland <jim@jbsoft.co.uk>
2020-01-29 13:07:34 +0000

    Hello Again added.

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -19,6 +19,8 @@ namespace HelloWorld
         private void btnHelloWorld_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello World.");
+
+            MessageBox.Show("Hello Again.");
         }
     }
 }
